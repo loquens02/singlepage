bf99a47cd3902e771edf0af3f7e2f221bab05fef
loquens02 <loquens20@hanyang.ac.kr>
2022-07-13 21:11:28 +0900

    SPA init-hello world!

---
+++ js/spa.js
@@ -0,0 +1,23 @@
+/*
+* spa.js
+* 루트 네임스페이스 모듈
+* */
+
+/*jslint            browser: true,      continue: true,
+devel: true,        indent: 2,          maxerr:     50,
+newcap: true,       nomen: true,        plusplus: true,
+regexp: true,       sloppy: true,       vars:     false,
+white:  true
+ */
+
+/*global $, spa */
+const spa = (function () {
+    const initModule = function ($container) {
+        $container.html(
+            '<h1 style="display:inline-block; margin:25px;">'
+            + 'hello world!'
+            + '</h1>'
+        )
+    }
+    return { initModule: initModule}
+})()
