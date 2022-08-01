311608f3a9a8f6ed9f2083bfa722fd4bcebfbffd
loquens02 <loquens20@hanyang.ac.kr>
2022-08-01 20:14:27 +0900

    add File_stub(껍데기)

---
+++ js/spa.data.js
@@ -0,0 +1,12 @@
+/*
+spa.data.js
+아바타 기능 모듈
+ */
+/*jslint         browser : true, continue : true,
+    devel  : true, indent  : 2,    maxerr   : 50,
+    newcap : true, nomen   : true, plusplus : true,
+    regexp : true, sloppy  : true, vars     : false,
+    white  : true
+ */
+/*global $, spa */
+spa.data= (function(){ return {}; })()
