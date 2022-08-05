bb11784a69214d3d36a2b09da6278ca03335f503
loquens02 <loquens20@hanyang.ac.kr>
2022-08-05 20:52:46 +0900

    remove code: 임의로 넣은 makeFakeCid 코드 제거. 아마도 여기에 CID 가 들어가는 건 로직상 적절하지 못한 듯

--- js/spa.fake.js
+++ js/spa.fake.js
@@ -14,13 +14,10 @@ spa.fake = (function () {
     // https://www.w3schools.com/js/js_strict.asp ECMA5 추가 사항. 변수나 함수 삭제 불가, 변수 키워드 없이 사용 불가 등
     // 스크립트나 함수의 '시작 부분에서만' 인식한다. IE 10+. 9 이하에서는 문자열로만 인식하므로 무시
 
-    let getPeopleList, makeFakeId, makeFakeCid, mockSio
+    let getPeopleList, makeFakeId, mockSio
     let fakeIdSerial = 5
     makeFakeId = function () {
-        return 'id_' + String(fakeIdSerial)
-    }
-    makeFakeCid = function () {
-        return String(fakeIdSerial)
+        return 'id_' + String(fakeIdSerial++)
     }
 
 
@@ -82,13 +79,11 @@ spa.fake = (function () {
                 setTimeout(function () {
                     callback_map.userupdate(
                         [{
-                            cid: makeFakeCid(),
                             id: makeFakeId(),
                             name: data.name,
                             css_map: data.css_map
                         }]
                     )
-                    fakeIdSerial++
                 }, 3000)
             }
         }
