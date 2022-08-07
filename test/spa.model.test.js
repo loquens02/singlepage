fedd7b1cd14ad72bd6ebe645cb480277fc552899
loquens02 <loquens20@hanyang.ac.kr>
2022-08-07 18:02:24 +0900

    uncomment: spa.model 의 logout 기능 테스트

--- test/spa.model.test.js
+++ test/spa.model.test.js
@@ -18,26 +18,26 @@ spa.model.test = (function () {
         const peopleDB = spa.model.people.get_db();
 
         peopleDB().each(function (person, idx) {
-            console.log(person.name)
+            // console.log(person.name)
         });
 
         spa.model.people.login('Alfred');
         currentUser = spa.model.people.get_user();
         console.log(currentUser.get_is_anonymous());
-
+        //
         console.log(currentUser.id);
 
         console.log(currentUser.cid);
-        //3초 대기
+        //3초 대기- Hello
 
-        // peopleDB().each(function(person, idx){console.log(person.name)});
-        //
-        // spa.model.people.logout()
-        //
-        // peopleDB().each(function(person, idx){console.log(person.name)});
-        //
-        // currentUser= spa.model.people.get_user();
-        // currentUser.get_is_anonymous()
+        peopleDB().each(function(person, idx){console.log(person.name)});
+
+        spa.model.people.logout()
+
+        peopleDB().each(function(person, idx){console.log(person.name)});
+
+        currentUser= spa.model.people.get_user();
+        console.log(currentUser.get_is_anonymous())
     }
 
     return {
