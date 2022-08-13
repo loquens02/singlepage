986c966500a130089f31327dc09f4f76e9321044
loquens02 <loquens20@hanyang.ac.kr>
2022-08-13 16:06:39 +0900

    mod code: (emit_sio)로그인 시 peopleList 에 사용자 추가

--- js/spa.fake.js
+++ js/spa.fake.js
@@ -14,48 +14,52 @@ spa.fake = (function () {
     // https://www.w3schools.com/js/js_strict.asp ECMA5 추가 사항. 변수나 함수 삭제 불가, 변수 키워드 없이 사용 불가 등
     // 스크립트나 함수의 '시작 부분에서만' 인식한다. IE 10+. 9 이하에서는 문자열로만 인식하므로 무시
 
-    let getPeopleList, makeFakeId, mockSio
+    let peopleList, makeFakeId, mockSio, getPeopleList
     let fakeIdSerial = 5
     makeFakeId = function () {
         return 'id_' + String(fakeIdSerial++)
     }
 
 
+
     /**
      * 가짜 사람 목록
      * @return {array}
      */
+    peopleList = [
+        {
+            cid: '1',
+            name: 'Betty',
+            id: 'id_01',
+            css_map: {top: 20, left: 20, 'background-color': 'rgb(128, 128, 128)'}
+        },
+        {
+            cid: '2',
+            name: 'Mike',
+            id: 'id_02',
+            css_map: {top: 60, left: 20, 'background-color': 'rgb(128, 255, 128)'}
+        },
+        {
+            cid: '3',
+            name: 'Pebbles',
+            id: 'id_03',
+            css_map: {top: 100, left: 20, 'background-color': 'rgb(128, 192, 192)'}
+        },
+        {
+            cid: '4',
+            name: 'Wilma',
+            id: 'id_04',
+            css_map: {top: 140, left: 20, 'background-color': 'rgb(192, 128, 128)'}
+        }
+    ]
+
     getPeopleList = function () {
-        return [
-            {
-                cid: '1',
-                name: 'Betty',
-                id: 'id_01',
-                css_map: {top: 20, left: 20, 'background-color': 'rgb(128, 128, 128)'}
-            },
-            {
-                cid: '2',
-                name: 'Mike',
-                id: 'id_02',
-                css_map: {top: 60, left: 20, 'background-color': 'rgb(128, 255, 128)'}
-            },
-            {
-                cid: '3',
-                name: 'Pebbles',
-                id: 'id_03',
-                css_map: {top: 100, left: 20, 'background-color': 'rgb(128, 192, 192)'}
-            },
-            {
-                cid: '4',
-                name: 'Wilma',
-                id: 'id_04',
-                css_map: {top: 140, left: 20, 'background-color': 'rgb(192, 128, 128)'}
-            }
-        ]
+        return peopleList
     }
 
     mockSio = (function () {
         let on_sio, emit_sio
+        let send_listchange, listchange_idto
         let callback_map = {}
 
         /**
@@ -75,15 +79,17 @@ spa.fake = (function () {
          * @function 메시지를 받으면 네트워크 반응 지연 시간을 시뮬레이션하기 위해 3초를 기다린 후 updateuser 콜백을 호출한다
          */
         emit_sio = function (msg_type, data) {
+            let person_map
             if (msg_type === 'adduser' && callback_map.userupdate) {
+                // 사용자가 로그인할 때 발생하는 adduser 메시지 응답 내용
                 setTimeout(function () {
-                    callback_map.userupdate(
-                        [{
-                            id: makeFakeId(),
-                            name: data.name,
-                            css_map: data.css_map
-                        }]
-                    )
+                    person_map = {
+                        id: makeFakeId(),
+                        name: data.name,
+                        css_map: data.css_map
+                    }
+                    peopleList.push(person_map)
+                    callback_map.userupdate([person_map])
                 }, 3000)
             }
         }
@@ -95,6 +101,8 @@ spa.fake = (function () {
     })()
 
     return {
+        // 이제 listchange 핸들러에서 필요한 데이터를 제공하므로 제거? (p.241 -> 264)
+        // ERROR p.239 spa.module.initModule 에서 쓰는데 없앤다? 그냥 냅둠
         getPeopleList: getPeopleList,
         mockSio: mockSio
     }
